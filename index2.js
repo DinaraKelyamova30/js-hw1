d8db1721e113be496c3046cda3a75ba036b03c96
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-10-30 16:00:15 +0200

    sample js

---
+++ index2.js
@@ -0,0 +1,4 @@
+let n1 = 9;
+let n2 = 3;
+result = n1 ** n2;
+console.log(result);
---
+++ index3.js
@@ -0,0 +1,8 @@
+let n3 = 4;
+let cost1 = 5.25;
+let sum1 = n3 * cost1;
+let n4 = 6;
+let cost2 = 3.50;
+let sum2 = n4 * cost2;
+let summarise = sum1 + sum2;
+document.write(summarise);
